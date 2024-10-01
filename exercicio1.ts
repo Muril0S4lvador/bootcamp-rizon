922797d491cdc8d41a2922777645898e3b51e3de
Murilo Salvador <murilo.salvador95@gmail.com>
2024-10-01 17:34:43 -0300

    feat: add exemplo parametro e forms

--- exercicio1.ts
+++ exercicio1.ts
@@ -1,5 +1,5 @@
-const leVogais = (palavra : string) : number => {
-    
+function contVogais (palavra : string) : number {
+
     var qtdVogais = 0;
     for( let p of palavra ){
         switch( p.toLowerCase() ){
@@ -15,13 +15,22 @@ const leVogais = (palavra : string) : number => {
     return qtdVogais;
 }
 
-document.getElementById('Formulario')?.addEventListener('submit', (event) => {
-    event.preventDefault();
-  
-    const palavraInput = document.getElementById('palavra') as HTMLInputElement;
-    const palavra = String(palavraInput.value);
-  
-    if (palavra != null) {
-        console.log(leVogais(palavra));
-    }
-});
+console.log(contVogais("RizOn BootCAmp"));
+
+const forms = document.getElementById('Formulario');
+if( forms ){
+    forms.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+        
+        const palavraInput = document.getElementById('palavra') as HTMLInputElement;
+        const palavra = String(palavraInput.value);
+        
+        if (palavra != null) {
+            console.log(contVogais(palavra));
+        }
+    });
+    
+} else {
+    console.log("Formulário não encontrado")
+};
